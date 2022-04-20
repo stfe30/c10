9a190ed955fadc9afcbd5b09ac17cc3d728c6078
stfe30 <101103094+stfe30@users.noreply.github.com>
2022-04-20 15:06:33 +0200

    Create c10.c

---
+++ c10.c
@@ -0,0 +1,23 @@
+ int main(void)
+ {
+     int *a, *b; /* Zeiger auf einen als Ganzzahl interpretierten Speicherbereich */
+ 
+     /* Speicher für Zeiger reservieren. */
+     a=malloc(sizeof(int));
+     b=malloc(sizeof(int));
+ 
+     *a = 5;     /* Schreibt den Wert „5“ in den von a referenzierten Speicherbereich */
+     *b = 3;     /* Schreibt den Wert „3“ in den von b referenzierten Speicherbereich */
+      a = b;     /* Weist dem Zeiger a das Ziel des Zeigers b zu. */
+ 
+     /* a und b verweisen nun auf den gleichen Speicherbereich. Der zuvor von a referenzierte
+      * Speicher ist damit verwaist und kann nicht mehr freigegeben werden. An dieser Stelle
+      * entsteht ein Speicherleck.
+      */
+ 
+     free(b);    /* Gibt den von b referenzierten Speicher frei */
+     free(a);    /* Führt zu einem Fehler, da der Bereich, auf den a
+                  * verweist, bereits freigegeben wurde (in der vorherigen Zeile)
+                  */
+     return EXIT_SUCCESS;
+ }
